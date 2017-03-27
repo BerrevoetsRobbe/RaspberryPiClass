73517e73415debdbb5274ac5e1641a2cc05d6638
robbe berrevoets <robbe.berrevoets@student.kuleuven.be>
2017-03-27 15:48:50 +0200

    add test for pinpad
    numpad crashes sometimes, try to fix this (maybe without event checkers)

--- Code/Sensors/BaseSensors/PinPad.py
+++ Code/Sensors/BaseSensors/PinPad.py
@@ -1,6 +1,5 @@
 from BaseSensor import BaseSensor
 from NumPad import NumPad
-import RPi.GPIO as GPIO
 import logging
 
 logger = logging.getLogger(__name__)
@@ -8,9 +7,11 @@ logger = logging.getLogger(__name__)
 
 class PinPad(BaseSensor):
 
-    def __init__(self, input_row_pins, input_column_pins, keys, pin, escape_key):
-        self.__numpad = NumPad(input_row_pins, input_column_pins, keys, self.register_key)
-        self.__pin = pin
+    def __init__(self, input_row_pins, input_column_pins, keys, pin, escape_key, buzzer):
+        logging.debug("PinPad created with pin {pin} and escape_key {escape_key}".format(pin=pin,
+                                                                                         escape_key=escape_key))
+        self.__numpad = NumPad(input_row_pins, input_column_pins, keys, buzzer, self.register_key)
+        self.__pin = [int(i) for i in str(pin)]
         self.__escape_key = escape_key
         self.__key_history = []
         self.__activated = -1
@@ -18,16 +19,18 @@ class PinPad(BaseSensor):
     def get_value(self):
         return self.__activated
 
-    def register_key(self):
-        key = self.__numpad.get_value() # object Key
+    def register_key(self, channel):
+        key = self.__numpad.detect(channel)
         if key is None:
             return
-        if key == self.__escape_key:
+        if key.get_value() == self.__escape_key:
             logger.debug("escape key entered")
             self.__key_history = []
         else:
-            logger.debug("key {key} entered".format(key=key.get_value()))
             self.__key_history.append(key.get_value())
+            logger.debug("key {key} entered, "
+                         "current key history is {key_history}".format(key=key.get_value(),
+                                                                       key_history=str(self.__key_history)))
 
         self.__check_key_history()
 
@@ -37,5 +40,8 @@ class PinPad(BaseSensor):
             self.__key_history = []
 
     def __pin_entered(self):
-        logger.info("pin entered")
         self.__activated *= -1
+        logger.info("pin entered: system is {active}active".format(active="not " if self.__activated==-1 else ""))
+
+    def destroy(self):
+        self.__numpad.destroy()
--- Code/TestFiles/PinPad.py
+++ Code/TestFiles/PinPad.py
@@ -1,2 +1,40 @@
+# arg1 = reed_pin, arg2 = r_pin, arg3 = g_pin
+import logging
+import RPi.GPIO as GPIO
+
+from Actuators.BaseActuators.Buzzer import Buzzer
+from Sensors.BaseSensors.NumKey import NumKey
+from Sensors.BaseSensors.PinPad import PinPad
+from Sensors.BaseSensors.SymbolKey import SymbolKey
+from time import sleep
+
+def create_key_list():
+    logging.debug("start key creation")
+    key_list = [NumKey(i) for i in range(1, 10)]
+    key_list.append(SymbolKey('*'))
+    key_list.append(NumKey(0))
+    key_list.append(SymbolKey('#'))
+    logging.debug("key list generated: {list}".format(list=key_list))
+    return key_list
+
+
 def test_pin_pad(args):
-    pass
+    row_pins = [int(arg) for arg in args[0:4]]
+    col_pins = [int(arg) for arg in args[4:7]]
+    buzzer_pin = int(args[7])
+    pin = int(args[8])
+    escape_key = args[9]
+    buzzer = Buzzer(buzzer_pin)
+    buzzer.start()
+    keys = create_key_list()
+    pin_pad = PinPad(row_pins, col_pins, keys, pin, escape_key, buzzer)
+    logging.info("created numpad: {pinpad}".format(pinpad=pin_pad))
+    try:
+        while True:
+            # yield processor
+            sleep(0.000001)
+    finally:
+        logging.info('Cleanup pinpad')
+        pin_pad.destroy()
+        sleep(2)
+        GPIO.cleanup()
